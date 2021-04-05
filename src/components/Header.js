3d3723b441e60a3c9b12954e011a7a88e8153c55
Nabil Lubega <nklubega@gmail.com>
2021-04-05 12:04:39 +0300

    Add files via upload

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,29 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
 
+const Header = ({ title }) => {
+    const onClick = () => {
+        console.log('click')
+    }
+    return (
+        <header className='header'>
+            <h1 style={headingStyle}>{title}</h1>
+            <Button color='green' text='Hello' onClick={onClick} />
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+//CSS in JS
+const headingStyle = {
+    textAlign: 'center',
+}
+
+export default Header
